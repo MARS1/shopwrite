a667becb99d6c7a5d17120b93942d32e23e6c363
FrankTrog <frank@frankt3d.com>
2014-06-11 11:51:05 -0400

    Work on issues with Marcelo. Slideup and item click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,49 @@
+var timeout; //Variable for timeout
+
+//Add timeout via function call
+function slideDownTimeout(){
+    timeout = setTimeout(function(){
+        //Add functionality to check if value in input. If so restart timer WITHOUT hiding input.
+        $('#writeModule').slideUp();
+    },1000);
+}
+
 $(document).ready(function(){
 
 	$('#writeModule').hide();
 	$('#addItemBtn').on('click', function() {
 		$('#writeModule').slideDown();
-	})
-	$('#writeModule').on('click', function(){
-		$(this).setTimeout(function(){
-			$(this).slideUp();
-		},7000);
+        slideDownTimeout(); //Add timeout
 	});
+    
+    //When key is pressed in input clear timeout and restart
+    $('#writeModule input').keydown(function(){
+        console.log('keyup');
+        clearTimeout(timeout); //Clear timeout
+        slideDownTimeout(); //Restart timeout
+    });
+    
 	// Trying to target li's inside of itemUl to set an event to be tiggered when the li is clicked I want the styles on the span and p tags to change when the li is clicked.
-	$('span.checkedOff').on('click', function(){
-		if($('span.checkedOff')) {
-			$(this).removeClass('checkedOff').addClass('checkedOn').child;
-			$(this).removeClass('listNeed').addClass('listGot');
-		}
+	$('.itemWrapper span').on('click', function(){
+        $(this).toggleClass('checkedOff checkedOn')
+            .siblings('p') //Targets siblings next to original target
+            .toggleClass('listNeed listGot'); //Toggle Class either adds or removes class based on whether it exists on element
+        console.log($(this)[0]); //Grabs HTML for target
 	});
+    
 	// Toggle Switch functions
 	var switchBtn = $('.switchBtn');
 	var switchTrack = $('.switchTrack');
 	$('p.need').on('click', function(){
-		switchBtn.css('left', '0');
+		switchBtn.css('left', '0'); //Would change to class and use toggle
 	})
 	$('p.got').on('click',function(){
-		switchBtn.css('left','49px');
+		switchBtn.css('left','49px'); //Would change to class and use toggle
 	})
+    switchTrack.click(function(){ //Suggested functionality for switch button
+        switchBtn.toggleClass('clicked');
+    });
+    
 	// When write button is clicked item is added to menu list
 
 	// Switch toggle needs to have additional interactions, click and drag slider button and press on opposite ends of the track to move it as well as give it a smoother transition effect.
